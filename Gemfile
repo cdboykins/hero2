26be1a139f98090121a66a81f87ed4081ab16c4c
cdboykins <carlosdboykins@icloud.com>
2019-06-03 20:02:35 -0400

    added random functions and route corrections

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
-
+gem 'faker', '~> 1.6', '>= 1.6.6'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+before_action :common, :only => [:index, :team]
+  def common
+    @hero_name = Faker::Superhero.name
+    @hero_power = Faker::Superhero.power
+    @hero_prefix = Faker::Superhero.prefix
+    @hero_suffix = Faker::Superhero.suffix
+    @hero-descriptor = Faker::Superhero.descriptor
+  end
+
   def index
   end
 
