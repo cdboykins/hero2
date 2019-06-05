17b71573a9fe2db17b5f7f979ef8f57737f972b7
cdboykins <carlosdboykins@icloud.com>
2019-06-05 00:57:10 -0400

    fourth heroku try

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/team'
-  get 'home/heroes'
+  #get 'home/heroes'
   post 'home/team' => 'home#team'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
